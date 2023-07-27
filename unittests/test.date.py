38219189649eb6c165f37e59b40126db79af9da2
Theo Jager (thuis) <theo@triplepro.nl>
2023-07-27 15:16:29 +0200

    [ new ][ date ] date functions with rollover attribute

---
+++ unittests/test.date.py
@@ -0,0 +1,24 @@
+import unittest
+
+from date.date import date
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_given_out_of_bound_dates__date__rolls_over_to_correct_date(self):
+        self.assertEqual( "20080328", date( 2008, 3, 28 ) )
+        self.assertEqual( "20080401", date( 2008, 3, 32 ) )
+        self.assertEqual( "20090101", date( 2008, 13, 1 ) )
+        self.assertEqual( "20100101", date( 2008, 25, 1 ) )
+        self.assertEqual( "20090304", date( 2008, 14, 32 ) )
+        self.assertEqual( "20080522", date( 2008, 2, 112 ) )
+        self.assertEqual( "20100120", date( 2008, 22, 112 ) )
+        self.assertEqual( "20080301", date( 2008, 2, 30 ) )
+        self.assertEqual( "20000301", date( 2000, 2, 30 ) )
+        self.assertEqual( "19000302", date( 1900, 2, 30 ) )
+
+if __name__ == '__main__':
+    unittest.main()
+
+# python -m unittest discover -s unittests -p "*.test.py" -v
+# python -m unittest discover -s unittests -v
