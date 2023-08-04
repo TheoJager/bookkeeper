49f54c2dbd682970bc453e1897c9053d18767c16
Theo Jager (thuis) <theo@triplepro.nl>
2023-08-04 16:10:23 +0200

    [ update ][ date ] move functions to class and add wrapper function

--- application/date/date.py
+++ application/date/date.py
@@ -1,58 +1,79 @@
 import datetime
 
-DAYS_IN_MONTH = [ -1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+# CONSTANTS
+#######################################
+
+FEBRUARY: int = 2
+
+DAYS_FEBRUARY_LEAPYEAR: int = 29
+DAYS_FEBRUARY_NON_LEAPYEAR: int = 28
+
+MONTHS_PER_YEAR: int = 12
+
+DAYS_IN_MONTH: list = [ -1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
 
 
-def is_leap( year ) -> bool:
-  return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
-
-
-def get_days_in_month( year, month ) -> int:
-  assert 1 <= month <= 12, month
-  if month == 2 and is_leap( year ):
-    return 29
-  return DAYS_IN_MONTH[ month ]
-
+# WRAPPER FUNCTION
+#######################################
 
 def date( year: int, month: int = 1, day: int = 1 ) -> str:
-  year, month = _rollover_months( year, month )
-  year, month, day = _rollover_days( year, month, day )
-
-  d = datetime.date( year, month, day )
-  return d.strftime( "%Y%m%d" )
+  d = RolloverDate( year, month, day )
+  return str( d )
 
 
-def _rollover_months( year: int, month: int ) -> list:
-  while month > 12:
-    month -= 12
-    year += 1
+# CLASS
+#######################################
 
-  while month < 1:
-    month += 12
-    year -= 1
+class RolloverDate:
+  year: int = 1
+  month: int = 1,
+  day: int = 1
 
-  return [ year, month ]
+  def __init__( self, year: int, month: int = 1, day: int = 1 ):
+    self.year, self.month = self._rollover_months( year, month )
+    self.year, self.month, self.day = self._rollover_days( self.year, self.month, day )
 
+  def __str__( self ) -> str:
+    d = datetime.date( self.year, self.month, self.day )
+    return d.strftime( "%Y%m%d" )
 
-def _rollover_days( year: int, month: int, day: int ) -> list:
-  if day > 28:
-    days_in_month = get_days_in_month( year, month )
-    while day > days_in_month:
-      days_in_month = get_days_in_month( year, month )
-      day -= days_in_month
-      month += 1
-  elif day == 0:
-    day = 1
-  elif day < 0:
-    while day < 0:
+  def _is_leap( self, year ) -> bool:
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+  def _get_days_in_month( self, year, month ) -> int:
+    assert 1 <= month <= MONTHS_PER_YEAR, month
+    if month == FEBRUARY and self._is_leap( year ):
+      return DAYS_FEBRUARY_LEAPYEAR
+    return DAYS_IN_MONTH[ month ]
+
+  def _rollover_months( self, year, month ) -> list:
+    while month < 1:
+      month = -1 if month == 0 else month
+      month += MONTHS_PER_YEAR + 1
+      year -= 1
+
+    while month > MONTHS_PER_YEAR:
+      month -= MONTHS_PER_YEAR
+      year += 1
+
+    return [ year, month ]
+
+  def _rollover_days( self, year, month, day ) -> list:
+    days_in_month = self._get_days_in_month( year, month )
+
+    while day < 1:
       month -= 1
-      year, month = _rollover_months( year, month )
-      days_in_month = get_days_in_month( year, month )
+      year, month = self._rollover_months( year, month )
+      days_in_month = self._get_days_in_month( year, month )
+
+      day = -1 if day == 0 else day
       day += days_in_month + 1
-      if day == 0:
-        year, month = _rollover_months( year, month - 1 )
-        day = get_days_in_month( year, month )
 
-  year, month = _rollover_months( year, month )
+    while day > days_in_month:
+      day -= days_in_month
 
-  return [ year, month, day ]
+      month += 1
+      year, month = self._rollover_months( year, month )
+      days_in_month = self._get_days_in_month( year, month )
+
+    return [ year, month, day ]
--- unittests/date/test.date.py
+++ unittests/date/test.date.py
@@ -3,27 +3,50 @@ import unittest
 from application.date.date import date
 
 
-class TestStringMethods(unittest.TestCase):
+class TestDate( unittest.TestCase ):
 
-    def test_given_out_of_bound_dates__date__rolls_over_to_correct_date(self):
-        self.assertEqual( "20080328", date( 2008, 3, 28 ) )
-        self.assertEqual( "20080401", date( 2008, 3, 32 ) )
-        self.assertEqual( "20090101", date( 2008, 13, 1 ) )
-        self.assertEqual( "20100101", date( 2008, 25, 1 ) )
-        self.assertEqual( "20090304", date( 2008, 14, 32 ) )
-        self.assertEqual( "20080522", date( 2008, 2, 112 ) )
-        self.assertEqual( "20100120", date( 2008, 22, 112 ) )
-        self.assertEqual( "20080301", date( 2008, 2, 30 ) )
-        self.assertEqual( "20000301", date( 2000, 2, 30 ) )
-        self.assertEqual( "19000302", date( 1900, 2, 30 ) )
-        self.assertEqual( "20080201", date( 2008, 2, 0 ) )
-        self.assertEqual( "20080131", date( 2008, 2, -1 ) )
-        self.assertEqual( "20080101", date( 2008, 2, -31 ) )
-        self.assertEqual( "20071231", date( 2008, 2, -32 ) )
-        self.assertEqual( "20081231", date( 2008, 12 + 1, -1 ) )
+  def test_given_out_of_bound_dates__date__rolls_over_to_correct_date( self ):
+    # up to 28th is normal
+    self.assertEqual( "20080328", date( 2008, 3, 28 ) )
+
+    # to many days and months rolls over
+    self.assertEqual( "20080522", date( 2008, 2, 112 ) )
+    self.assertEqual( "20100101", date( 2008, 25, 1 ) )
+    self.assertEqual( "20100120", date( 2008, 22, 112 ) )
+
+    # to many days in leapyear february rolls over over to 1st
+    self.assertEqual( "20080301", date( 2008, 2, 30 ) )
+    self.assertEqual( "20000301", date( 2000, 2, 30 ) )
+
+    # to manu days in none leapyear february rolls over to 2nd
+    self.assertEqual( "19000302", date( 1900, 2, 30 ) )
+
+    # zero day adjusts to last day|month
+    self.assertEqual( "20080131", date( 2008, 2, 0 ) )
+    self.assertEqual( "20071201", date( 2008, 0, 1 ) )
+
+    # -1 days is last day previous month
+    self.assertEqual( "20080131", date( 2008, 2, -1 ) )
+    # -1 month is last month previous year
+    self.assertEqual( "20071201", date( 2008, -1, 1 ) )
+
+    # minus previous results in 1st
+    self.assertEqual( "20080101", date( 2008, 2, -31 ) )
+    self.assertEqual( "20070101", date( 2008, -12, 1 ) )
+
+    # minus previous + 1 results in 1st - 1
+    self.assertEqual( "20071231", date( 2008, 2, -32 ) )
+    self.assertEqual( "20061201", date( 2008, -13, 1 ) )
+
+    # +1 month -1 day == last day current month
+    self.assertEqual( "20081231", date( 2008, 12 + 1, -1 ) )
+
+    # -1 month + days_in_month + 1
+    self.assertEqual( "20080101", date( 2008, -1, 32 ) )
+
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
 
 # python -m unittest discover -s unittests -p "*.test.py" -v
 # python -m unittest discover -s unittests -v
