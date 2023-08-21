99ab96d502f0dc258140c84f5ae8c02c08f61a04
Theo Jager (thuis) <theo@triplepro.nl>
2023-08-21 13:18:23 +0200

    [ update ][ main ] undo virtual env ( too much hassle, doesn't work, no benefit )

--- application/main.py
+++ application/main.py
@@ -14,8 +14,8 @@ from database.database_categories import Database_Categories
 
 # COMPILE
 #######################################
-# pyinstaller --onedir  --noconsole --clean --collect-all venv/Lib/site-packages/customtkinter --icon=application/pennytracker.ico --name pennytracker --upx-dir=venv/upx application/main.py
-# pyinstaller --onefile --noconsole --clean --collect-all venv/Lib/site-packages/customtkinter --icon=application/pennytracker.ico --name pennytracker --upx-dir=venv/upx application/main.py
+# pyinstaller --onedir  --noconsole --clean --collect-all customtkinter --icon=application/pennytracker.ico --name pennytracker --upx-dir=venv/upx application/main.py
+# pyinstaller --onefile --noconsole --clean --collect-all customtkinter --icon=application/pennytracker.ico --name pennytracker --upx-dir=venv/upx application/main.py
 
 # DATABASE SETUP
 #######################################
